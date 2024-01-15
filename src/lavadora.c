ceea42ef284fcea45009f296bcb339171b604d8d
Alonso J Jiménez A <alonso.jimenezanchia@ucr.ac.cr>
2024-01-14 22:52:26 -0600

    Interrupción por botón ON/OFF ya funciona. Se presiona y se enciende LED de Suministro de Agua. Se coloca pull-down a entrada INT0 en el circuito

--- src/lavadora.c
+++ src/lavadora.c
@@ -35,14 +35,10 @@ ISR(INT0_vect){
     PORTB |= (1 << PB7);
 }
 
-
 /*** MAIN ***/
 
 int main(void)
 {
-    // Se habilita interrupción global
-    sei();
-
     // Se configuran entradas y salidas en puerto B
     DDRB = 0xFF;
 
@@ -50,22 +46,25 @@ int main(void)
     PORTB = 0x00;
 
     // Se configuran entradas y salidas en puerto A
-    //DDRA 
+    DDRA = 0xFF;
 
     // Se inicializan entradas en puerto A
-    //DDRB = 0x00;
+    PORTA = 0x00;
 
     // Se configuran entradas y salidas en puerto D
     DDRD = 0x73;
 
     // Se inicializan entradas en puerto D
-    PORTD |= (1 << ISC01 )
-
-    // Se habilita la interrupcion externa por INT0
-    GIMSK |= (1 << INT0 ); 
+    PORTD |= (0<<PORTD6)|(0<<PORTD5)|(0<<PORTD4)|(0<<PORTD3)|(0<<PORTD2)|(0<<PORTD2)|(0<<PORTD1)|(0<<PORTD0);
+    
+    // Se habilita la interrupcion externa para INT0
+    GIMSK |= (1<<INT0); 
 
     // EL flanco creciente en INT0 genera la interrupcion
     MCUCR |= (1 << ISC01 )|(1 << ISC00 );
+
+    // Se habilita interrupción global
+    sei();
 
     while(1)
     {
